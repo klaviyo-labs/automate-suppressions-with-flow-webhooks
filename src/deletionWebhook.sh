f4c920c416ad36486c2bbc103792584e07510215
Michaela <michaela.fooksa@klaviyo.com>
2025-07-03 10:27:39 -0600

    updated private API key

--- src/deletionWebhook.sh
+++ src/deletionWebhook.sh
@@ -1,11 +1,10 @@
 # to create a webhook to delete Klaviyo profiles, use the following setup.
 # for more information on the Request Profile Deletion API endpoint, see https://developers.klaviyo.com/en/reference/request_profile_deletion
 
-PRIVATE_API_KEY="your_private_api_key_here" # Replace with your actual private API key
-
+# replace your-private-api-key with your actual Klaviyo private API key that has write access to data-privacy.
 curl --request POST \
      --url https://a.klaviyo.com/api/data-privacy-deletion-jobs \
-     --header "Authorization: Klaviyo-API-Key $PRIVATE_API_KEY" \
+     --header "Authorization: Klaviyo-API-Key your-private-api-key" \
      --header "accept: application/vnd.api+json" \
      --header "content-type: application/vnd.api+json" \
      --header "revision: 2025-04-15" \
--- src/suppresionWebhook.sh
+++ src/suppresionWebhook.sh
@@ -1,11 +1,10 @@
 # to create a webhook to suppress Klaviyo profiles, use the following setup.
 # for more information on the Bulk Suppress Profile API endpoint, see https://developers.klaviyo.com/en/reference/bulk_suppress_profiles
 
-PRIVATE_API_KEY="your_private_api_key_here" # Replace with your actual private API key
-
+# replace your-private-api-key with your actual Klaviyo private API key that has write access to profiles and subscriptions.
 curl --request POST \
      --url https://a.klaviyo.com/api/profile-suppression-bulk-create-jobs \
-     --header "Authorization: Klaviyo-API-Key $PRIVATE_API_KEY" \
+     --header "Authorization: Klaviyo-API-Key your-private-api-key" \
      --header "accept: application/vnd.api+json" \
      --header "content-type: application/vnd.api+json" \
      --header "revision: 2025-04-15" \
